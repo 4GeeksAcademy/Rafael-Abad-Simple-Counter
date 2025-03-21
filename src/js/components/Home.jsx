c2344069b6259ed34f69db94bca48e17ca181ea4
Rafael468 <abadginerrafa@gmail.com>
2025-03-21 19:25:46 +0000

    Initial commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,31 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [seconds, setSeconds]= useState(0);
+	
+useEffect(()=>{
+	const counter = setInterval(()=>{
+		setSeconds((inSeconds)=>inSeconds+1);
+	},1000);
+	return ()=>clearInterval(counter);
+},[
+	//dependency empty, only runs once per load
+]);
+const formattedTime = String(seconds).padStart(6, "0");
+
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1><FontAwesomeIcon icon={faClock} className="clock-icon" />{formattedTime}</h1>
 		</div>
 	);
 };
